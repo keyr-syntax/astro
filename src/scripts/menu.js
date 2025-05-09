360297d955a0464aa7cfc4ca3615a30b05ac9c52
Keyru Nasir <keyrsyntax@gmail.com>
2025-05-09 15:44:55 +0300

    Dynamic routing

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,22 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('expanded');
-  });
+const BACKEND_API = "https://test-subdomain.keyrunasir.com";
+
+const fetchallblogs = async () => {
+  try {
+    const data = await fetch(`${BACKEND_API}/api/blog/fetchallblogs`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const response = await data.json();
+    if (response.success === true) {
+      setBlogcontent(response.post);
+    } else if (response.success === false) {
+      setBlogcontent([]);
+    }
+  } catch (error) {
+    console.log("Error while fetching all blogs", error);
+  }
+};
+
+//Dynamic routing: to={`/readblog/${blog.id}`}
