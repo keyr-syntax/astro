ae428a4ef29d3445fcdb668a4245a044b2efec5c
Keyru Nasir <keyrsyntax@gmail.com>
2025-05-09 17:53:14 +0300

    Hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+export default defineConfig({
+  output: 'server',     
+});
 
-// https://astro.build/config
-export default defineConfig({});
+
